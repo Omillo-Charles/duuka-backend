5acd572e99b2c47fbdf7c6769eade6051e23e06d
Omillo Fidel Charles <fidelomillo812@gmail.com>
2026-06-05 01:15:20 +0300

    fix: initialize cookie parser and correct prisma client generator provider

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser);
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
     res.send("Welcome to the dotSoko ExpressJS Backend API");
